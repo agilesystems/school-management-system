eb4d3c401cd9524b56aaca1b4471809fefdaf17e
Ramy Ashour <eng.ramy.ashour@gmail.com>
2017-04-05 13:59:29 +0200

    successfully loaded static resources

---
+++ schoolmanagementsystem/src/main/java/com/agile/schoolmanagementsystem/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.agile.schoolmanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+	
+		ModelAndView mav = new ModelAndView("masterpage");
+		mav.addObject("msg","Welcom");
+		return mav;
+		
+	}
+	
+}
